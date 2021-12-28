2cfeb6ba4ac2687a33d9ec5654b97b64a14edb37
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-12-28 23:26:30 +0300

    adding social media icons, introducing the click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 // alert("my Name")
+
+$(document).ready(function(){
+    $(".design").click(function(){
+        $(".design-hidden").toggle();
+        $(".design-showing").toggle();
+
+    });
+});
+$(document).ready(function(){
+    $(".development").click(function(){
+        $(".development-hidden").toggle();
+        $(".development-showing").toggle();
+
+    });
+});
