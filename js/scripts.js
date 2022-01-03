f6e886d34d804a7e788ede4ea8b953ba08176621
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-01-03 13:46:41 +0300

    adjusting response message

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,29 @@ $(document).ready(function() {
 });
 $(document).ready(function(){
 $(".submit").click(function(){
-    var Name = $('#mce-FNAME').val();
-    var Email = $('#mce-EMAIL').val();
-    var Message = $('#mce-MESSAGE');
+    var Name = $("#mce-FNAME").val();
+    var Email = $("#mce-EMAIL").val();
+    var Message = $("#mce-MESSAGE");
     var key ="b417dfc18ca6a0e998f3fc8642553d4b-us20";
-    if (Name == '' || Email == '' || Message == '') {
+    if (Name == " " || Email == " " || Message == " ") {
         alert("ok, but fill in all spaces correctly");
     } else {
         alert("Hi " + Name + " We have received your message. Thank you for contacting us");
     }
 });
-// $(".Hover").hover(function(){
-//     $(this).animate({opacity:'1'}, 100,);
-// }, 
-//     function(){
-//     $(this).animate({opacity:'0'}, 100);
-// });
 });
+
+// $(document).ready(function(){
+// $(".submit").click(function(event){
+// var Name = $("#mce-FNAME").val();
+//     var Email = $("#mce-EMAIL").val();
+//     var Message = $("#mce-MESSAGE");
+//     var key ="b417dfc18ca6a0e998f3fc8642553d4b-us20";
+//     if ($("#mce-FNAME").val() && $("#mce-EMAIL").val() && ("#mce-MESSAGE").val()) {
+// 	alert (Name + ", we have received your message. Thank you for reaching out to us.");
+//     } else {
+//         alert("Please enter all details!");
+//     }
+// });
+// });
+
